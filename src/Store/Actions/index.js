1728c26a8206fdd954ada1729154c298db1c2a0b
naveedaheer <naveedaheer@gmail.com>
2017-04-05 16:59:09 +0500

    signUp method added

--- src/Store/Actions/index.js
+++ src/Store/Actions/index.js
@@ -1,4 +1,28 @@
 import {Constants} from '../Constants'
+import {firebaseApp} from '../../Database/firebaseApp'
+
+
+export function RegisterUser(SignUpData) {
+    return dispatch => {
+        dispatch(userSignUp());
+        firebaseApp.Auth().createUserWithEmailAndPassword(SignUpData.email, SignUpData.password)
+            .then((data) => {
+                const userRef = firebaseApp.database.ref('/users/' + data.uid);
+                userRef.set({
+                    uid : data.uid,
+                    email: data.email,
+                    name: SignUpData.fullname 
+                }, signUpSuccessData => {
+                    dispatch(userSignUpSuccess({ uid: data.uid, userEmail: data.email, name: SignUpData.name, gender: SignUpData.gender == 1 ? "Male" : "Female", address: SignUpData.address, bloodType: SignUpData.bloodType,  }));
+                });
+            })
+            .catch((error) => {
+                console.log("Something Went Wrong, Please Try Again ", error);
+                dispatch(userSignUpFailed (error));
+            });
+    }   
+}
+
 
 //LoggedIn
 export function hasLoggedIn(email){
@@ -56,6 +80,20 @@ export function userSignInFailed(error){
     const action = {
         type: Constants.SIGN_IN_FAILED,
         error
+    }
+    return action;
+}
+
+export function userLogOut(){
+    const action = {
+        type: Constants.LOG_OUT
+    }
+    return action;
+}
+
+export function userLogOutSuccess(){
+    const action = {
+        type: Constants.LOG_OUT_SUCCESS
     }
     return action;
 }
