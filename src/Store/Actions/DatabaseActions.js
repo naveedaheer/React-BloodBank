5c09a6cd712993489081aa6ccb413f3bcbaa2371
naveedaheer <naveedaheer@gmail.com>
2017-04-08 13:00:58 +0500

    signin query method completed

--- src/Store/Actions/DatabaseActions.js
+++ src/Store/Actions/DatabaseActions.js
@@ -1,5 +1,6 @@
 import AllActions from './AllActions'
 import {firebaseApp} from '../../Database/firebaseApp'
+import {browserHistory} from 'react-router'
 
 export function RegisterUser(SignUpData) {
     console.log("RegisterUser(SignUpData)", SignUpData)
@@ -28,12 +29,25 @@ export function RegisterUser(SignUpData) {
 }
 
 export function LoginUser(LogInData){
-        console.log("RegisterUser(SignUpData)", SignUpData)
+        console.log("LoginUser(LogInData)", LogInData)
         return (dispatch) =>{
             dispatch(AllActions.userSignIn());
             firebaseApp.auth().signInWithEmailAndPassword(LogInData.email, LogInData.password)
             .then((data)=>{
-                
+                return firebaseApp.database().ref('/userInfo'+data.uid).once('value', snapshot=>{
+                    let userData = snapshot.val();
+                    console.log("userData", userData);
+                    dispatch(AllActions.userSignInSuccess(userData))
+                    browserHistory.replace('/home')
+                })
+
+            })
+            .catch((error)=>{
+                console.log("login error", error)
+                dispatch(AllActions.userSignInFailed(error))
             })
         }
 }
+
+export function RegisterDonor(){}
+export function ViewDonors(){}
