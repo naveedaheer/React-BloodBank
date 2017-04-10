04aed819d6a0964a2eea05258034e19caf1c69e8
naveedaheer <naveedaheer@gmail.com>
2017-04-10 18:56:05 +0500

    renamed

---
+++ src/Store/Reducers/reducer.js
@@ -0,0 +1,78 @@
+import {firebaseApp, ref} from '../../Database/firebaseApp'
+import {Constants} from '../Constants'
+
+
+const INITIAL_STATE = {
+    hasError: false,
+    errorMessage: {},
+    hasLoggedIn: false,
+    hasRegistered: false,
+    user:{},
+    donor:{},
+}
+
+export var authReducer = (state=INITIAL_STATE, action) => {
+    console.log("action in reducer action", action)
+
+    switch(action.type){
+
+        case Constants.HAS_SIGNED_IN:
+        const {email} = action;
+        return email
+
+        case Constants.SIGN_UP:
+       return Object.assign({}, state, {user: action.signUpData} )
+
+        case Constants.SIGN_UP_SUCCESS:
+       return Object.assign({}, state, {user: action.authUserData} )
+
+        case Constants.SIGN_UP_FAILED:
+       return Object.assign({}, state, {user: action.signUpData}, {hasError:true}, {errorMessage:action.error} )
+
+         case Constants.SIGN_IN:
+       return Object.assign({}, state, {user: action.signInData} )
+
+        case Constants.SIGN_IN_SUCCESS:
+       return Object.assign({}, state, {user: action.authUserData})
+
+        case Constants.SIGN_IN_FAILED:
+        return Object.assign({}, state, {hasError:true, errorMessage:action.error})
+
+        case Constants.LOG_OUT:
+       return Object.assign({}, state, {user: action.data})
+
+        case Constants.LOG_OUT_SUCCESS:
+       return Object.assign({}, state, {user: action.data})
+
+        default:
+        return state
+    }
+}
+
+export var donorReducer = (state= INITIAL_STATE, action) => {
+    console.log("action in donor reducer", action)
+    switch(action.type){
+    
+       case Constants.REGISTER_DONOR:
+       return Object.assign({}, state )
+
+       case Constants.REGISTER_DONOR_SUCCESS:
+       return Object.assign({}, state, {user: action.RegisterDonorData})
+
+       case Constants.REGISTER_DONOR_FAILED:
+       return Object.assign({}, state, {hasError:true, errorMessage:action.error})
+
+       case Constants.VIEW_DONOR:
+       return Object.assign({}, state)
+
+       case Constants.VIEW_DONOR_SUCCESS:
+       return Object.assign({}, state, {donor: action.donorsList})
+
+       case Constants.VIEW_DONOR_FAILED:
+       return Object.assign({}, state, {hasError:true, errorMessage:action.error})
+
+
+        default:
+        return state
+    }
+}
